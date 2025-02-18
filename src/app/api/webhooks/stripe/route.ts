a8658278f299991c35235f685a3779336ee15d24
Thsering Sherpa <devshering@gmail.com>
2025-02-18 07:47:37 +0100

    debug:  console to debug strip webhook

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -62,6 +62,7 @@ export async function POST(req: Request) {
 
                 // Cancel previous subscription
                 if (session?.metadata?.previous_subscription) {
+                    console.log(`Cancel previous subscription`)
                     await stripe.subscriptions.update(
                         session.metadata.previous_subscription,
                         { 
